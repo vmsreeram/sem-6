4636898d6f02c9175a93a5037b36a6c615ea805f
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-02-14 16:17:32 +0530

    interpolation qn done

--- Computational Methods and Applications/assignment3/Q5.py
+++ Computational Methods and Applications/assignment3/Q5.py
@@ -1,47 +1,72 @@
 import numpy as np
+from scipy.interpolate import interp1d, CubicSpline, Akima1DInterpolator, BarycentricInterpolator
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
 from matplotlib.animation import FuncAnimation
 
-# Define a function to be interpolated
-def func(x):
-    return np.tan(x)+np.sin(30*x)+np.exp(x)
+# Define the function to be interpolated
+def f(x):
+    return np.tan(x)*np.sin(30*x)*np.exp(x)
 
-# Define the interpolation methods to be used
-interp_methods = ['linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic']
+# Define the number of sampling points for the interpolation
+num_points = [i for i in range(2,36)]
 
-# Generate x values for the original function
-x = np.linspace(0, 10, num=100)
-y = func(x)
 
-# Initialize the plot
+# Define the interpolation functions
+interpolation_functions = [
+                            CubicSpline
+                           , Akima1DInterpolator
+                           , BarycentricInterpolator
+                           ]
+interpolation_names = [
+                        'Cubic Spline'
+                       , 'Akima'
+                       , 'Barycentric'
+                       ]
+
+interpolation_colors = [
+                         'r'
+                        , 'g'
+                        , 'purple'
+                        ]
+
+# Set up the plot
 fig, ax = plt.subplots()
-ax.set_xlim(0, 10)
-ax.set_ylim(-8, 18)
-ax.set_title('Interpolation Methods')
+ax.set_xlim(0, 1.0)
+ax.set_ylim(-4.0, 4.0)
 
-# Initialize the lines and points to be animated
-lines = []
-points = []
-for method in interp_methods:
-    line, = ax.plot([], [], '-', label=method)
-    lines.append(line)
-    point, = ax.plot([], [], 'o', color=line.get_color(), markersize=3)
-    points.append(point)
 
-ax.legend()
-
-# Define the animation function
+# Define the update function for the animation
 def update(frame):
-    for i, method in enumerate(interp_methods):
-        # Interpolate the function using the specified method
-        num_points = 10 * (frame + 1)
-        x_interp = np.linspace(0, 10, num=num_points)
-        y_interp = interp1d(x, y, kind=method)(x_interp)
-        lines[i].set_data(x_interp, y_interp)
-        points[i].set_data(x_interp, y_interp)
+    ax.clear()
+    # ax.set_xlim(0, 1.0)
+    ax.set_ylim(-4.0, 4.0)
 
-# Create the animation
-ani = FuncAnimation(fig, update, frames=np.arange(10), interval=500)
+    num = num_points[frame]
+    ax.set_title(r'Different interpolations for $tan(x) \cdot sin(30x) \cdot e^x$'+ ' for {} samples'.format(num))
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$f(x)$')
+    ax.grid()
+    
+    x = np.linspace(0, 1.0, 500)
+    y = f(x)
+    ax.plot(x, y, label='True')
+    
+    # Generate the data for the plot for the current frame
+    x = np.linspace(0, 1.0, num)
+    y = f(x)
 
-plt.show()
+
+    # Plot the different interpolation functions
+    for i, fn in enumerate(interpolation_functions):
+        f_int = fn(x, y)
+        x1=np.linspace(0, 1.0, 500)
+        ax.plot(x1, f_int(x1), label=interpolation_names[i], c=interpolation_colors[i])
+
+    # Add the legend
+    ax.legend(loc='upper left')
+
+# Generate the initial data for the plot
+
+# Create and save the animation
+anim = FuncAnimation(fig, update, frames=len(num_points), repeat=False)
+anim.save('interpolation_animation.gif', writer='pillow')
