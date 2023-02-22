bb22ba284fc22e75dccb804779dba39dc082749b
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-02-22 15:43:49 +0530

    Q3 fine

--- Computational Methods and Applications/assignment4/Q3.py
+++ Computational Methods and Applications/assignment4/Q3.py
@@ -1,3 +1,4 @@
+# Plese note that this problem took ~5s to produce the output
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,11 +14,7 @@ def f_dash(x):
 def f_doubledash(x):
     return 2*(-2*x*x*np.sin(x**2) + np.cos(x**2))
 
-# Defining the triple derivative of f(x)
-def f_tripledash(x):
-    return -4*x*(2*x*x*np.cos(x**2) + 3*np.sin(x**2))
-
-# Defining the approximations of f(x) and populating required variables
+# Defining the approximations and populating required variables
 def delta_plus(x, h):
     return (f(x + h) - f(x)) / h
 def delta_c(x, h):
@@ -26,10 +23,10 @@ def delta_c(x, h):
 x_values = np.linspace(0, 1, 100)
 y_f_dash = f_dash(x_values)
 
-def compute_maxAbsErrs(h):
-    y_delta_plus = delta_plus(x_values, h)
-    y_delta_plus_absErr = np.abs(y_delta_plus - y_f_dash)
-    ans_delta_plus = max(y_delta_plus_absErr)
+def compute_maxAbsErrs(h):                                          # this function returns max absolute errors of delta+ and deltac for a given h
+    y_delta_plus = delta_plus(x_values, h)                          # computing delta+
+    y_delta_plus_absErr = np.abs(y_delta_plus - y_f_dash)           # absolute error will be abs of diff
+    ans_delta_plus = max(y_delta_plus_absErr)                       # maximum over x in 0 to 1 (as np.linspace(0, 1, 100))
 
     y_delta_c = delta_c(x_values, h)
     y_delta_c_absErr = np.abs(y_delta_c - y_f_dash)
@@ -37,13 +34,20 @@ def compute_maxAbsErrs(h):
     
     return ans_delta_plus,ans_delta_c
 
-def compute_theoreticalErrors(h):
+def compute_theoreticalErrors(h):                                   # this function returns max theoretical absolute errors of delta+ and deltac for a given h
     max_deltaPlusErr_th,max_deltaCErr_th = 0,0
     for x in np.linspace(0,1,100):
-        max_doubleder_err = max(abs(f_doubledash(xx)) for xx in np.linspace(x,x+h,100))
-        max_tripleder_err = max(abs(f_tripledash(xx)) for xx in np.linspace(x,x+h,100))
-        max_deltaPlusErr_th = max(max_deltaPlusErr_th,(h/2)*max_doubleder_err)
-        max_deltaCErr_th = max(max_deltaCErr_th,(h**2/6)*max_tripleder_err)
+        # computing max and min double derivative error in [x,x+h] and [x-h,x].
+        max_doubleder_err_x_xplush = max((f_doubledash(xx)) for xx in np.linspace(x,x+h,100))
+        min_doubleder_err_x_xplush = min((f_doubledash(xx)) for xx in np.linspace(x,x+h,100))
+        max_doubleder_err_xminush_x = max((f_doubledash(xx)) for xx in np.linspace(x-h,x,100))
+        min_doubleder_err_xminush_x = min((f_doubledash(xx)) for xx in np.linspace(x-h,x,100))
+        
+        # max theoretical abs error for delta+ is (h/2) max(f"(c))over c in x to x+h
+        max_deltaPlusErr_th = max(max_deltaPlusErr_th,(h/2)*(max(abs(max_doubleder_err_x_xplush),abs(min_doubleder_err_x_xplush))))
+        
+        # max theoretical abs error for deltac is (h/4) max(f"(c1) - f"(c2))over c1 in x to x+h and c1 in x-h to x
+        max_deltaCErr_th = max(max_deltaCErr_th,(h/4)*(max(abs(max_doubleder_err_x_xplush-min_doubleder_err_xminush_x),abs(min_doubleder_err_x_xplush-max_doubleder_err_xminush_x))) )
     return max_deltaPlusErr_th,max_deltaCErr_th
 
 # Generating data for plotting
@@ -62,12 +66,13 @@ for h in hs:
 
 # Plotting...
 plt.plot(hs, maxErr_delta_plus, label="maximum absolute error in forward difference")
-plt.plot(hs, maxErr_delta_c, label="maximum absolute error in central difference")
 plt.plot(hs, theoErr_delta_plus, label="theoretical maximum absolute error in forward difference")
-# plt.plot(hs, theoErr_delta_c, label="theoretical maximum absolute error in central difference")
+plt.plot(hs, maxErr_delta_c, label="maximum absolute error in central difference")
+plt.plot(hs, theoErr_delta_c, label="theoretical maximum absolute error in central difference")
 plt.legend()
-plt.title("maximum absolute error of approximations for " +r"$sin(x^2)$")
+plt.title("Maximum absolute error of approximations for " +r"$sin(x^2)$")
 plt.xlabel("h")
 plt.ylabel(r"$\epsilon$")
 plt.grid()
 plt.show()
+# Plese note that this problem took ~5s to produce the output
